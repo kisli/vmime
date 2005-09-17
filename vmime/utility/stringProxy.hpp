cbd1110a4bd05590b98c4a5f2a4739c807d3b64a
Vincent Richard <vincent@vincent-richard.net>
2005-09-17 10:10:29 +0000

    Updated FSF address.

--- vmime/utility/stringProxy.hpp
+++ vmime/utility/stringProxy.hpp
@@ -12,9 +12,13 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Linking this library statically or dynamically with other modules is making
+// a combined work based on this library.  Thus, the terms and conditions of
+// the GNU General Public License cover the whole combination.
 //
 
 #ifndef VMIME_UTILITY_STRINGPROXY_HPP_INCLUDED
