5280d12555d1580161832ab43f0523b16a27b7bb
Vincent Richard <vincent@vincent-richard.net>
2006-02-05 10:30:52 +0000

    Changed copyright year to 2006.

--- vmime/relay.hpp
+++ vmime/relay.hpp
@@ -1,6 +1,6 @@
 //
 // VMime library (http://www.vmime.org)
-// Copyright (C) 2002-2005 Vincent Richard <vincent@vincent-richard.net>
+// Copyright (C) 2002-2006 Vincent Richard <vincent@vincent-richard.net>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
