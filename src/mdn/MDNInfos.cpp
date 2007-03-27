a88d21a336d20cda74b079456efa38bf097eac5e
Vincent Richard <vincent@vincent-richard.net>
2007-03-27 12:42:53 +0000

    Fixed typo in the copyright header, and added missing paragraph about linking.

--- src/mdn/MDNInfos.cpp
+++ src/mdn/MDNInfos.cpp
@@ -12,9 +12,13 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along along
-// with this program; if not, write to the Free Software Foundation, Inc., Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Linking this library statically or dynamically with other modules is making
+// a combined work based on this library.  Thus, the terms and conditions of
+// the GNU General Public License cover the whole combination.
 //
 
 #include "vmime/mdn/MDNInfos.hpp"
