0c30c298da468de4e1abd1589028ce0c1aaf8505
Vincent Richard <vincent@vincent-richard.net>
2008-01-04 18:07:40 +0000

    Changed copyright year to 2008.

--- vmime/constants.hpp
+++ vmime/constants.hpp
@@ -1,6 +1,6 @@
 //
 // VMime library (http://www.vmime.org)
-// Copyright (C) 2002-2007 Vincent Richard <vincent@vincent-richard.net>
+// Copyright (C) 2002-2008 Vincent Richard <vincent@vincent-richard.net>
 //
 // This program is free SOFTWARE; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
