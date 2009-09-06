a5d258dc724e5819647799f0e134d4138e620975
Vincent Richard <vincent@vincent-richard.net>
2009-09-06 12:02:10 +0000

    Relicensed VMime under the GNU GPL version 3. Changed copyright year to 2009.

--- tests/parser/mediaTypeTest.cpp
+++ tests/parser/mediaTypeTest.cpp
@@ -1,10 +1,10 @@
 //
 // VMime library (http://www.vmime.org)
-// Copyright (C) 2002-2008 Vincent Richard <vincent@vincent-richard.net>
+// Copyright (C) 2002-2009 Vincent Richard <vincent@vincent-richard.net>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of
+// published by the Free Software Foundation; either version 3 of
 // the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
